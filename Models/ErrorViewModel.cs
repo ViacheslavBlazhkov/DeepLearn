ce517414dcc4a9b58834278da2ada6964b3a4c2e
Viacheslav Blazhkov <blazkov253@gmail.com>
2023-03-05 09:44:36 +0200

    ok

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeepLearn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
